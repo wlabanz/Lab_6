abc42d4eab271d638b0939fb3b7bc5d1c2d07a07
wlabanz <148989689+wlabanz@users.noreply.github.com>
2023-10-25 11:37:54 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# Will Labanz - encoder and main function
+newPassword = ''
+userOption = 0
+passwordInput = ''
+
+def encode(integer_string):
+    encodedPassword = ''
+    for i in integer_string:
+        new_digit = int(i) + 3
+        if new_digit > 9:
+            new_digit = new_digit % 10
+            encodedPassword = encodedPassword + str(new_digit)
+        else:
+            encodedPassword = encodedPassword + str(new_digit)
+    return encodedPassword
+
+
+if __name__ == '__main__':
+    while userOption != 3:
+        print("Menu", "-" * 13, "1. Encode", "2. Decode", "3. Quit\n", sep='\n')
+        userOption = int(input("Please enter an option:"))
+        if userOption == 1:
+            passwordInput = input("Please enter your password to encode:")
+            newPassword = encode(passwordInput)
+            print("Your password has been encoded and stored!\n")
